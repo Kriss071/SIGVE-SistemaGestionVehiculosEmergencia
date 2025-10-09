d754dc5f5c0694b9bb220b0d184b3b9c6ab7c277
Kriss071 <chrisquinga@gmail.com>
2025-10-09 14:37:49 -0300

    Mejora del Toast

--- web/vehicles/static/js/vehicles.js
+++ web/vehicles/static/js/vehicles.js
@@ -1,8 +1,34 @@
-const modal = document.getElementById("vehicle_modal");
-document.getElementById("btnAddVehicle").onclick = () => modal.style.display = "block";
-function closeModal() { modal.style.display = "none"; }
 
+//  Manejar el modal del formulario de vehículos
+
+const modal = document.getElementById("vehicle_modal");
+
+document.getElementById("btnAddVehicle").onclick = () => modal.style.display = "block";
+
+function closeModal() {
+    modal.style.display = "none";
+}
 
 window.onclick = function (event) {
     if (event.target == modal) modal.style.display = "none";
 }
+
+// Toast Control
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toasts = document.querySelectorAll(".toast");
+    const delay = 4000;
+    const appearDelay = 500;
+
+    toasts.forEach((toast, index) => {
+        
+        // Animación de entrada
+        setTimeout(() => toast.classList.add("show"), appearDelay + (200 * index));
+
+        // Animación de salida y eliminación
+        setTimeout(() => {
+            toast.classList.add("hide");
+            setTimeout(() => toast.remove(), 500);
+        }, delay + (index * 200));
+    });
+});
