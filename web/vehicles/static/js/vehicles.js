45c2e9008aa44484f167b682acfb791000d52844
Kriss071 <chrisquinga@gmail.com>
2025-10-10 16:22:06 -0300

    Formulario de creación de vehiculos con estilos

--- web/vehicles/static/js/vehicles.js
+++ web/vehicles/static/js/vehicles.js
@@ -1,27 +1,29 @@
-
 //  Manejar el modal del formulario de vehículos
-
 const modal = document.getElementById("vehicle_modal");
+const btnAdd = document.getElementById("btnAddVehicle");
 
-document.getElementById("btnAddVehicle").onclick = () => modal.style.display = "block";
+btnAdd.onclick = () => {
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden";
+}
 
 function closeModal() {
     modal.style.display = "none";
+    document.body.style.overflow = "";
 }
 
 window.onclick = function (event) {
-    if (event.target == modal) modal.style.display = "none";
+    if (event.target == modal) closeModal();
 }
 
 // Toast Control
-
 document.addEventListener("DOMContentLoaded", function () {
     const toasts = document.querySelectorAll(".toast");
     const delay = 4000;
     const appearDelay = 500;
 
     toasts.forEach((toast, index) => {
-        
+
         // Animación de entrada
         setTimeout(() => toast.classList.add("show"), appearDelay + (200 * index));
 
