017ceb8ec6081af077815cda2bbdc297f7ed263e
Kriss071 <chrisquinga@gmail.com>
2025-10-19 01:33:29 -0300

    Se aplico el diseño del LoginScreen, se agrego imagen al proyecto login_background.png

--- mobile/app/src/main/java/com/capstone/sigve/ui/auth/LoginScreen.kt
+++ mobile/app/src/main/java/com/capstone/sigve/ui/auth/LoginScreen.kt
@@ -1,29 +1,122 @@
 package com.capstone.sigve.ui.auth
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.capstone.sigve.ui.navigation.AppScreens
+import com.capstone.sigve.R
 
 @Composable
 fun LoginScreen(navController: NavController) {
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
     ) {
-        Text("LOGIN SCREEN")
-        Button(onClick = {
-            navController.navigate(AppScreens.SettingsScreen.route) {}
-        }) {
-            Text("Configuraciones")
+        Image(
+            painter = painterResource(id = R.drawable.login_background),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Bienvenido a SIGVE", fontSize = 40.sp, fontWeight = FontWeight.Bold, color = Color.White)
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Card(
+                shape = RoundedCornerShape(20.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                colors = CardDefaults.cardColors(
+                    contentColor = Color.Black,
+                    containerColor = Color.White,
+                )
+            ) {
+                Column(
+                    modifier = Modifier.padding(24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Text(
+                        text = "Iniciar Sesión", fontWeight = FontWeight.Bold, fontSize = 32.sp
+                    )
+
+                    OutlinedTextField(value = "",
+                        onValueChange = {},
+                        label = { Text("Usuario") },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Default.Person, contentDescription = "User Icon"
+                            )
+                        },
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(100)
+                    )
+
+                    OutlinedTextField(value = "",
+                        onValueChange = {},
+                        label = { Text("Contraseña") },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Default.Lock, contentDescription = "User Icon"
+                            )
+                        },
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(100)
+                    )
+
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp),
+                        shape = RoundedCornerShape(100),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFDF2532)
+                        )
+                    ) {
+                        Text("Iniciar Sesión", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White)
+                    }
+
+                    TextButton(onClick = {}) {
+                        Text("Olvide mi contraseña", color = Color(0xFF83a2ff))
+                    }
+                }
+            }
         }
     }
 }
